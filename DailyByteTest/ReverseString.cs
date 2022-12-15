70cb30ca18820717402e178aa838b2f9790f4123
Haruka Kotani <harukakotani28@gmail.com>
2022-12-14 21:11:07 -0800

    added ReverseString

---
+++ DailyByteTest/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte
+{
+    public class ReverseString
+    {
+        //This question is asked by Google.
+        //Given a string, reverse all of its characters and return the resulting string.
+        //Ex: Given the following strings...
+        //“Cat”, return “taC”
+        //“The Daily Byte”, return "etyB yliaD ehT”
+        //“civic”, return “civic”
+        public static string GetReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ DailyByteTest/ReverseStringTest.cs
@@ -0,0 +1,23 @@
+using DailyByte;
+using Xunit;
+
+namespace DailyByteTest
+{
+    public class ReverseStringTest
+    {
+        [Theory]
+        [InlineData("Cat", "taC")]
+        [InlineData("The Daily Byte", "etyB yliaD ehT")]
+        [InlineData("civic", "civic")]
+        public void ReverseStringTestWithInputSamples(string input, string expected)
+        {
+            //arrange
+            //act
+            string result = ReverseString.GetReverseString(input);
+
+            // assert
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
