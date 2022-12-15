939c28a686360b981df3eb9f67d7fd30c6362afd
Haruka Kotani <harukakotani28@gmail.com>
2022-12-14 20:56:11 -0800

    added ReverseString

---
+++ DailyByte/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString.GetReverseString("Cat"));
+        }
+    }
+}
