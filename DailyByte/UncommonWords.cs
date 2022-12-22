0deba927ab795721a9326f8224d28ed100945230
Haruka Kotani <harukakotani28@gmail.com>
2022-12-22 15:15:33 -0800

    added UncommonWords

---
+++ DailyByte/UncommonWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DailyByte
+{
+    public class UncommonWords
+    {
+        //This question is asked by Amazon. 
+        //Given two strings representing sentences, 
+        //return the words that are not common to both strings 
+        //(i.e. the words that only appear in one of the sentences). 
+        //You may assume that each sentence is a sequence of words (without punctuation) correctly separated using space characters.
+        //
+        //Ex: given the following strings...
+        //
+        //sentence1 = "the quick", sentence2 = "brown fox", return ["the", "quick", "brown", "fox"]
+        //sentence1 = "the tortoise beat the haire", sentence2 = "the tortoise lost to the haire", return ["beat", "to", "lost"]
+        //sentence1 = "copper coffee pot", sentence2 = "hot coffee pot", return ["copper", "hot"]
+
+        public static string [] GetUncommonWords(string sentence1, string sentence2)
+        {
+            string combine = sentence1 + " " + sentence2;
+            string[] result = combine.Split(' ');
+            var list = new List<string>();
+
+            foreach(string i in result)
+                if (result.Count(r => r.Equals(i)) == 1)
+                    list.Add(i);
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ DailyByteTest/UncommonWordsTest.cs
@@ -0,0 +1,23 @@
+using DailyByte;
+using System;
+using Xunit;
+
+namespace DailyByteTest
+{
+    public class UncommonWordsTest
+    {
+        [Theory]
+        [InlineData("the quick", "brown fox", new string[] { "the", "quick", "brown", "fox" })]
+        [InlineData("the tortoise beat the haire", "the tortoise lost to the haire", new string[] { "beat", "lost", "to" })]
+        [InlineData("copper coffee pot", "hot coffee pot", new string[] { "copper", "hot" })]
+        public void GetUncommonWordsWithSampleInput(string sentence1, string sentence2, string[] expected)
+        {
+            //arrange
+            //act
+            string[] result = UncommonWords.GetUncommonWords(sentence1, sentence2);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
