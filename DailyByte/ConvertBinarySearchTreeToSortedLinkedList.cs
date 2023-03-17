5d5fce28046af57893dd0869eaf55d8bdad312a7
Haruka Kotani <harukakotani28@gmail.com>
2023-03-16 22:54:52 -0700

    added ConvertBinarySearchToSortedLinkedList

---
+++ DailyByte/ConvertBinarySearchTreeToSortedLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte
+{
+    public class ConvertBinarySearchTreeToSortedLinkedList<T>
+    {
+        public static LinkedList<TreeNode> ConvertTree(TreeNode root)
+        {
+            LinkedList<TreeNode> sortedList = new LinkedList<TreeNode>();
+            ConvertTreeHelper(root, sortedList);
+            return sortedList;
+        }
+
+        private static void ConvertTreeHelper(TreeNode node, LinkedList<TreeNode> list)
+        {
+            if (node == null)
+                return;
+
+            ConvertTreeHelper(node.left, list);
+            list.AddLast(node);
+            ConvertTreeHelper(node.right, list);
+        }
+    }
+}
--- DailyByteTest/FindValueTest.cs
+++ DailyByteTest/FindValueTest.cs
@@ -27,5 +27,29 @@ namespace DailyByteTest
             node = FindValue.SearchBST(root, 5);
             Assert.Equal(null, node);
         }
+
+        [Fact]
+        public void TestConvertTree()
+        {
+            // Create a binary search tree
+            TreeNode tree = new TreeNode(4);
+            tree.left = new TreeNode(2);
+            tree.left.left = new TreeNode(1);
+            tree.left.right = new TreeNode(3);
+            tree.right = new TreeNode(6);
+            tree.right.left = new TreeNode(5);
+            tree.right.right = new TreeNode(7);
+
+            // Convert the tree to a linked list
+            LinkedList<TreeNode> sortedList = ConvertBinarySearchTreeToSortedLinkedList<int>.ConvertTree(tree);
+
+            // Check if the linked list is sorted in ascending order
+            LinkedListNode<TreeNode> node = sortedList.First;
+            while (node.Next != null)
+            {
+                Assert.True(node.Value.val < node.Next.Value.val);
+                node = node.Next;
+            }
+        }
     }
 }
