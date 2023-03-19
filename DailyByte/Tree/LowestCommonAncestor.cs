35a9e843ffda47c180f0ac38fa0ef25a79f30ade
Haruka Kotani <harukakotani28@gmail.com>
2023-03-19 15:30:33 -0700

    created 'Tree' folder'

---
+++ DailyByte/Tree/LowestCommonAncestor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    public class LowestCommonAncestor
+    {
+        //Given a binary search tree that contains unique values and two nodes within the tree, a, and b, return their lowest common ancestor.
+        //Note: the lowest common ancestor of two nodes is the deepest node within the tree such that both nodes are descendants of it.
+        public static TreeNode GetLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+            {
+                return null;  // base case: reached the end of a subtree without finding p or q
+            }
+
+            if (p.val < root.val && q.val < root.val)
+            {
+                return GetLowestCommonAncestor(root.left, p, q);  // search the left subtree
+            }
+
+            if (p.val > root.val && q.val > root.val)
+            {
+                return GetLowestCommonAncestor(root.right, p, q);  // search the right subtree
+            }
+
+            return root;  // root is the LCA because p and q are on different sides of it
+        }
+    }
+}
---
+++ DailyByte/Tree/SortedArrayToBinarySearchTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    public class SortedArrayToBinarySearchTree
+    {
+        public static TreeNode ConvertArrayToBST(int [] nums)
+        {
+            return Helper(nums, 0, nums.Length - 1);
+        }
+
+        public static TreeNode Helper(int [] nums, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            int mid = (start + end) / 2;
+            TreeNode node = new TreeNode(nums[mid]);
+
+            node.left = Helper(nums, 0, mid - 1);
+            node.right = Helper(nums, mid + 1, end);
+
+            return node;
+        }
+    }
+}
