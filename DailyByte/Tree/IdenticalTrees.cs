acbe30507a5dd3bf4fb50969274db2898e47661e
Haruka Kotani <harukakotani28@gmail.com>
2023-03-20 21:36:13 -0700

    added IdenticalTrees

---
+++ DailyByte/Tree/IdenticalTrees.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    public class IdenticalTrees
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            return Helper(p, q);
+        }
+
+        public static bool Helper(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            if (p.val != q.val)
+                return false;
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
