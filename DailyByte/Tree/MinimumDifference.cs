00c8e0ef4e7d955bb95589f284054c65c13b6d6e
Haruka Kotani <harukakotani28@gmail.com>
2023-03-21 20:32:37 -0700

    added minimum difference

---
+++ DailyByte/Tree/MinimumDifference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    public class MinimumDifference
+    {
+        public static int GetMiniDif(TreeNode root)
+        {
+            int min = int.MaxValue;
+            int prev = int.MinValue;
+
+             GetMin(root, ref min, ref prev);
+
+            return min;
+        }
+
+        public static void GetMin(TreeNode node, ref int min, ref int prev)
+        {
+            if (node == null)
+                return;
+
+            GetMin(node.left, ref min, ref prev);
+
+            if(prev != int.MinValue)
+            {
+                int diff = node.val - prev;
+                if (diff < min)
+                    min = diff;
+            }
+
+            prev = node.val;
+            GetMin(node.right, ref min, ref prev);    
+            
+        }
+    }
+}
