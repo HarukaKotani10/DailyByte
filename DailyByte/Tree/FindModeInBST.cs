df832fb841e40b2225e04c245293993b5331d948
Haruka Kotani <harukakotani28@gmail.com>
2023-03-22 21:26:37 -0700

    added FindModeInBST

---
+++ DailyByte/Tree/FindModeInBST.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    public class FindModeInBST
+    {
+        public int FindMode(TreeNode root)
+        {
+            Dictionary<int, int> myDict = new Dictionary<int, int>();
+            if (root == null)
+                return -1;
+
+            Helper(myDict, root);
+            int maxVal = myDict.Values.Max();
+
+            foreach(int i in myDict.Keys)
+            {
+                if (myDict[i] == maxVal)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void Helper(Dictionary<int, int> myDict, TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            Helper(myDict, root.left);
+
+            if (myDict.ContainsKey(root.val))
+                myDict[root.val] = myDict[root.val] + 1;
+
+            else
+                myDict.Add(root.val, 1);
+
+            Helper(myDict, root.right);
+        }
+    }
+}
