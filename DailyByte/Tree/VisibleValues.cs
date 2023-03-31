d6f62d4f7931965c850adbdc1c26fcb952785c1f
Haruka Kotani <harukakotani28@gmail.com>
2023-03-30 21:49:15 -0700

    added GetFisibleValues

---
+++ DailyByte/Tree/VisibleValues.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    class VisibleValues
+    {
+        public List<int> GetFisibleValues(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root == null)
+                return result;
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                int size = q.Count;    
+                result.Add(q.Peek().val);
+
+                for(int i=0; i < size; i++)
+                {
+                    var node = q.Dequeue();
+                    if (node.left != null)
+                        q.Enqueue(node.left);
+                    if (node.right != null)
+                        q.Enqueue(node.right);
+                }    
+            }
+
+            return result;
+        }
+    }
+}
