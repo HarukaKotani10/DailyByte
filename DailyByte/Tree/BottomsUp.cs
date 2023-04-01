99056df8f234e7a8fd7cbadae457b2b14030f2f0
Haruka Kotani <harukakotani28@gmail.com>
2023-03-31 22:00:24 -0700

    added bottomsup class

---
+++ DailyByte/Tree/BottomsUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyByte.Tree
+{
+    class BottomsUp
+    {
+        public List<List<int>> GetBottomsUp(TreeNode root)
+        {
+            var result = new List<List<int>>();
+            var tempQueue = new Queue<TreeNode>();
+
+            if (root == null)
+                return result;
+            
+            tempQueue.Enqueue(root);
+
+            while(tempQueue.Count > 0)
+            {
+                int size = tempQueue.Count;
+                var tempList = new List<int>();
+
+                for (int i=0; i < size; i++)
+                {
+                    TreeNode node = tempQueue.Dequeue();
+                    tempList.Add(node.val);
+
+                    if (node.left != null)
+                        tempQueue.Enqueue(node.left);
+
+                    if (node.right != null)
+                        tempQueue.Enqueue(node.right);
+                }
+
+                result.Insert(0, tempList);
+            }
+
+            return result;
+        }
+    }
+}
